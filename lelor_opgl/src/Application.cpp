aba5d36ec934abe87552d9c3658a5acd9e74ec77
Lelor <lrodrigues0804@gmail.com>
2021-08-17 00:52:54 -0300

    setting up vertex array objects to manage draw context

--- lelor_opgl/src/Application.cpp
+++ lelor_opgl/src/Application.cpp
@@ -10,16 +10,18 @@
 
 std::string GetShaderCode(std::string path)
 {
-    std::ifstream shaderData(path);
-    std::stringstream buffer;
-    buffer << shaderData.rdbuf();
-    return buffer.str();
+    std::ifstream shaderCode(path);
+    std::stringstream strBuffer;
+    strBuffer << shaderCode.rdbuf();
+    return strBuffer.str();
 }
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
+    /* Get GLSL shader raw codeand load it into OpenGL compiler.
+       source can be any of the GLenum shader types */
     unsigned int id = glCreateShader(type);
-    const char* src = source.c_str();
+    const char* src = &source[0];
     glShaderSource(id, 1, &src, nullptr);
     glCompileShader(id);
 
@@ -30,7 +32,7 @@ static unsigned int CompileShader(unsigned int type, const std::string& source)
     {
         int length;
         glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* message = (char*)alloca(length * sizeof(char));
+        char* message = (char*)_malloca(length * sizeof(char));
         glGetShaderInfoLog(id, length, &length, message);
         std::cout << "FAILED TO COMPILE SHADER" << std::endl;
         std::cout << message << std::endl;
@@ -68,7 +70,7 @@ int main(void)
 
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -78,51 +80,76 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
     if (glewInit() != GLEW_OK)
-        std::cout << "hehe xd" << std::endl;
+        std::cout << "Failed to initalize GLEW" << std::endl;
 
-    float positions[] = {
-        -0.5f, -0.5f,
-         0.5f, -0.5f,
-         0.5f,  0.5f,
-        -0.5f,  0.5f,
+    // vertex data that will be allocated to GL_ARRAY_BUFFER.
+    // The layout as defined later is only 2 * sizeof(float) per vertice
+    // containing only the coordinates for now
+    float firstTriangle[] = {
+        -0.9f, -0.5f,  // left
+        -0.0f, -0.5f,  // right
+        -0.45f, 0.5f,  // top
+    };
+
+    float secondTriangle[] = {
+        0.0f, -0.5f,  // left
+        0.9f, -0.5f,  // right
+        0.45f, 0.5f   // top
     };
 
     unsigned int indices[] = {
         0, 1, 2,
-        2, 3, 0
     };
 
+    /* Creating buffer for vertex array objects */
+    unsigned int vbo[2], vao[2];
+    glGenVertexArrays(2, vao);
+    glGenBuffers(2, vbo);
 
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBindVertexArray(vao[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, 3 * 2 * sizeof(float), firstTriangle, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+    glBindVertexArray(vao[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glBufferData(GL_ARRAY_BUFFER, 3 * 2 * sizeof(float), secondTriangle, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
     unsigned int ibo;
     glGenBuffers(1, &ibo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
-    unsigned int shader = CreateShader(GetShaderCode("res/shaders/Basic.vert"), GetShaderCode("res/shaders/Basic.frag"));
-    glUseProgram(shader);
+    unsigned int redShader = CreateShader(GetShaderCode("res/shaders/Basic.vert"), GetShaderCode("res/shaders/Red.frag"));
+    unsigned int blueShader = CreateShader(GetShaderCode("res/shaders/Basic.vert"), GetShaderCode("res/shaders/Blue.frag"));
+    glBindVertexArray(0);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        glUseProgram(redShader);
+        glBindVertexArray(vao[0]);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+        glUseProgram(blueShader);
+        glBindVertexArray(vao[1]);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
     }
-
-    glDeleteProgram(shader);
+    
+    glDeleteVertexArrays(2, vao);
+    glDeleteBuffers(2, vbo);
+    glDeleteProgram(redShader);
+    glDeleteProgram(blueShader);
     glfwTerminate();
     return 0;
 }
