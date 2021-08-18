828ad25541772e6c11cf7a82438c258e120f8227
Lelor <lrodrigues0804@gmail.com>
2021-08-18 11:44:45 -0300

    Fixing vertex data attribute pointer stride size and processing color by a vertex attribute on the vertex shader

--- lelor_opgl/src/Application.cpp
+++ lelor_opgl/src/Application.cpp
@@ -86,59 +86,52 @@ int main(void)
     // The layout as defined later is only 2 * sizeof(float) per vertice
     // containing only the coordinates for now
     float firstTriangle[] = {
-        -0.9f, -0.5f,  // left
-        -0.0f, -0.5f,  // right
-        -0.45f, 0.5f,  // top
-    };
-
-    float secondTriangle[] = {
-        0.0f, -0.5f,  // left
-        0.9f, -0.5f,  // right
-        0.45f, 0.5f   // top
+        // first triangle
+        -0.9f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f,  // left
+        -0.0f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // right
+        -0.45f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // top
+        // second triangle
+         0.0f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, // left
+         0.9f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, // right
+         0.45f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f // top
     };
 
     unsigned int indices[] = {
         0, 1, 2,
+        3, 4, 5
     };
 
     /* Creating buffer for vertex array objects */
-    unsigned int vbo[2], vao[2];
-    glGenVertexArrays(2, vao);
-    glGenBuffers(2, vbo);
+    unsigned int vbo, vao;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
 
-    glBindVertexArray(vao[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-    glBufferData(GL_ARRAY_BUFFER, 3 * 2 * sizeof(float), firstTriangle, GL_STATIC_DRAW);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 6 * sizeof(float), firstTriangle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
-
-    glBindVertexArray(vao[1]);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-    glBufferData(GL_ARRAY_BUFFER, 3 * 2 * sizeof(float), secondTriangle, GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)(2*sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     unsigned int ibo;
     glGenBuffers(1, &ibo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
-    unsigned int redShader = CreateShader(GetShaderCode("res/shaders/Basic.vert"), GetShaderCode("res/shaders/Red.frag"));
-    unsigned int blueShader = CreateShader(GetShaderCode("res/shaders/Basic.vert"), GetShaderCode("res/shaders/Blue.frag"));
-    glBindVertexArray(0);
+    //unsigned int redShader = CreateShader(GetShaderCode("res/shaders/Basic.vert"), GetShaderCode("res/shaders/Red.frag"));
+    //unsigned int blueShader = CreateShader(GetShaderCode("res/shaders/Basic.vert"), GetShaderCode("res/shaders/Blue.frag"));
+    unsigned int genericShader = CreateShader(GetShaderCode("res/shaders/Basic.vert"), GetShaderCode("res/shaders/Basic.frag"));
+    glUseProgram(genericShader);
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glUseProgram(redShader);
-        glBindVertexArray(vao[0]);
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
-        glUseProgram(blueShader);
-        glBindVertexArray(vao[1]);
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
-
+        //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
@@ -146,10 +139,9 @@ int main(void)
         glfwPollEvents();
     }
     
-    glDeleteVertexArrays(2, vao);
-    glDeleteBuffers(2, vbo);
-    glDeleteProgram(redShader);
-    glDeleteProgram(blueShader);
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteProgram(genericShader);
     glfwTerminate();
     return 0;
 }
